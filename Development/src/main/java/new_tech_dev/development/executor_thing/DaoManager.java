04860f419e6553ae78b69500858056b63fd1b775
mcouto <mcouto@GO040026.itdeusto.local>
2018-02-08 15:18:32 +0100

    Commit Message

---
+++ Development/src/main/java/new_tech_dev/development/executor_thing/DaoManager.java
@@ -0,0 +1,5 @@
+package new_tech_dev.development.executor_thing;
+
+
+public class DaoManager {
+}
---
+++ Development/src/main/java/new_tech_dev/development/return_thing/ReturnFactory.java
@@ -0,0 +1,9 @@
+package new_tech_dev.development.return_thing;
+
+public class ReturnFactory {
+	
+	public ReturnFactory(Class<?> clazz){
+		
+	}
+	
+}
