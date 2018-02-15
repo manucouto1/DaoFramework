8e8fc4292f42119ba6a79e81b277b960fe2b577d
manuc <manuc@192.168.0.161>
2018-02-15 21:28:14 +0100

    pre versioning

--- Development/src/main/java/new_tech_dev/development/method_thing/MethodProcessor.java
+++
@@ -1,5 +0,0 @@
-package new_tech_dev.development.method_thing;
-
-public interface MethodProcessor {
-	public void process(String action);
-}
--- Development/src/main/java/new_tech_dev/development/proxy_thing/ComplexCacheableTreeObject.java
+++
@@ -1,9 +0,0 @@
-package new_tech_dev.development.proxy;
-
-import new_tech_dev.development.base_entity.BaseEntity;
-
-public class ComplexCacheableTreeObject extends BaseEntity{
-	
-	
-
-}
